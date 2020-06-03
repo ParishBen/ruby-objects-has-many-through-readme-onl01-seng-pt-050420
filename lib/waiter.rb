801b5380f843ae3c5037b29027243ce1716d3861
Ben Parish <parishben13@gmail.com>
2020-06-03 04:01:06 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -6,17 +6,22 @@ def initialize(name, age)
   @age = age
   @@all << self
  end
+
  def self.all 
    @@all
  end
+
  def new_meal(customer, total, tip)
    Meal.new(self,customer,total,tip)
  end
+ 
  def meals
    Meal.all.select {|meal| meal.waiter==self}
  end
+ 
  def best_tipper
-   meals.collect {|meal| sorted= [] 
+   sorted= [] 
+   meals.collect {|meal|
    sorted << meal.tip
    really = sorted.sort 
    if meal.tip == really.last 
