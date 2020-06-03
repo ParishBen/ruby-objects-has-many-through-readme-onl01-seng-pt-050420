72c82cec69bf6790b9a8fbd59bcd069b6fd55f45
Ben Parish <parishben13@gmail.com>
2020-06-03 04:12:16 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,10 +20,10 @@ def initialize(name, age)
  end
  
  def best_tipper
-   def self.oldest_customer
-    oldest_age = 0
-    oldest_customer = nil
-    self.all.each do |customer|
+   
+    largest_tip = 0
+    best_tipping_customer = nil
+    meals.each do |customer|
       if customer.age > oldest_age
         oldest_age = customer.age
         oldest_customer = customer
